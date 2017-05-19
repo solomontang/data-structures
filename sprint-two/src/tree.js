9bf0548e4afc38df677cc657bc90bb73961d4653
solomontang <solomoncztang@gmail.com>
2017-05-18 20:47:00 -0700

    TODO: Fix tree.contains to properly recurse over all children

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -15,10 +15,30 @@ treeMethods.addChild = function(value) {
 };
 
 treeMethods.contains = function(target) {
-  
+  debugger;
+  //pattern should reflect getElementsByClassName
+  //use a recursive function to set a contain flag
+  //return flag
+  var containFlag = false;
+  var checkChildren = function (tree) {
+    
+    if (tree.value === target) { 
+      containFlag = true;
+    }
+    if (tree.children.length > 0) {
+      console.log('what');
+      //recurse over this function and adjust flag as necessary.
+      _.each(tree.children, function(child) { 
+        checkChildren(child);   
+      });
+    }
+  };
+
+  return containFlag;
 };
 
-
+var tree = new Tree();
+tree.addChild(5);
 
 /*
  * Complexity: What is the time complexity of the above functions?
