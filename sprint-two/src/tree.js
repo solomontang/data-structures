f2433efcef0ca95be203d38757ca0988d2f13591
solomontang <solomoncztang@gmail.com>
2017-05-19 10:18:09 -0700

    Implemented tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,7 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
   _.extend(newTree, treeMethods);
-  // your code here
-  newTree.children = [];  // fix me 
+  newTree.children = [];
 
   return newTree;
 };
@@ -15,10 +14,7 @@ treeMethods.addChild = function(value) {
 };
 
 treeMethods.contains = function(target) {
-  debugger;
-  //pattern should reflect getElementsByClassName
-  //use a recursive function to set a contain flag
-  //return flag
+
   var containFlag = false;
   var checkChildren = function (tree) {
     
@@ -26,19 +22,13 @@ treeMethods.contains = function(target) {
       containFlag = true;
     }
     if (tree.children.length > 0) {
-      console.log('what');
-      //recurse over this function and adjust flag as necessary.
-      _.each(tree.children, function(child) { 
-        checkChildren(child);   
-      });
+      tree.children.forEach(child => checkChildren(child));
     }
   };
-
+  checkChildren(this);
   return containFlag;
 };
 
-var tree = new Tree();
-tree.addChild(5);
 
 /*
  * Complexity: What is the time complexity of the above functions?
